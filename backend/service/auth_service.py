69189c95341a8d96bc4c4dc955aaf25a5d2d06c1
sparsh2002 <>
2025-01-22 08:53:02 +0530

    destructured code to service level

---
+++ backend/service/auth_service.py
@@ -0,0 +1,31 @@
+from flask import request, jsonify
+from flask_jwt_extended import create_access_token
+import datetime
+def signup(db , User, bcrypt):
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+    email = data.get('email')
+
+    if User.query.filter_by(username=username).first():
+        return jsonify({'message': 'User already exists'}), 400
+
+    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+    new_user = User(username=username, password=hashed_password, email=email)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'message': 'User registered successfully'}), 201
+
+
+def login(User, bcrypt):
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+
+    user = User.query.filter_by(username=username).first()
+    if not user or not bcrypt.check_password_hash(user.password, password):
+        return jsonify({'message': 'Invalid credentials'}), 401
+
+    access_token = create_access_token(identity=user.id, expires_delta=datetime.timedelta(days=1))
+    return jsonify({'access_token': access_token}), 200
